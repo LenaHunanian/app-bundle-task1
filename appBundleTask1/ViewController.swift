8b71c4205541837c8b805ae62f7a207f17f09aa8
LenaHunanian <lenasss20053112@gmail.com>
2025-07-01 17:43:16 +0400

    Update ViewController.swift view background color fix

--- appBundleTask1/ViewController.swift
+++ appBundleTask1/ViewController.swift
@@ -21,6 +21,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
         setupLabels()
         setupTextViews()
         setupButtons()
